9b9663fdb22688b0f3bc4a595d353efcb8a69484
Angel <rriera96@gmail.com>
2016-06-17 23:45:03 -0400

    added some styling

--- browser/js/modalWindow/modal.controller.js
+++ browser/js/modalWindow/modal.controller.js
@@ -1,7 +1,7 @@
 app.controller('ModalInstanceCtrl', function ( $scope, $compile, $uibModalInstance) {
 
   $scope.ok = function () {
-    $uibModalInstance.close();
+    $uibModalInstance.close($scope.counter);
   };
 
   $scope.cancel = function () {
@@ -10,44 +10,10 @@ app.controller('ModalInstanceCtrl', function ( $scope, $compile, $uibModalInstan
 
 
   $scope.setTimer= function(){
+    // $scope.$broadcast('timer-start');
+    $scope.counter= ($scope.hours*60 + $scope.minutes)*60;
+    console.log($scope.buddy+' '+$scope.counter);
     $scope.$broadcast('timer-start');
-    // if(!$scope.minutes) $scope.minutes=0;
-    // var date = new Date();
-    // var hour= date.getHours();
-    // var mins= date.getMinutes();
-    // var time= {};
-
-    // console.log(hour, mins);
-
-    // function checkTime(hr, min){
-    //   var hrToMin= hr/60;
-    //   if(mins+min>59){
-    //     time.min= mins+min - 60;
-    //     hour++;
-    //     time.hour++;
-    //   }
-    //   else time.min= mins+min;
-    //   if(hour+hr>23) time.hour= hour+hr -24;
-    //   else time.hour= hour+hr
-    // }
-
-    // checkTime($scope.hours, $scope.minutes);
-    // console.log(time);
-
-    // $('.countdown').countEverest({
-    //   day: 17,
-    //   month: 6,
-    //   year: 2016,
-    //   hour: time.hour,
-    //   minute: time.min,
-    //   second: 0,
-    //   millisecond: 0,
-    //   onComplete: function(){
-    //     console.log("TIMER IS UP!");
-    //   }
-    // });
-
-
     $scope.ok();
 
   }
--- browser/js/timers/timers.controller.js
+++ browser/js/timers/timers.controller.js
@@ -1,14 +1,30 @@
 'use strict'
 
 app.controller('TimersController', function($scope, $uibModal){
-	$scope.hours= 0;
+
+	$scope.counter=10;
+
+	$scope.logIt= function(){
+		$scope.$broadcast('timer-reset');
+		$scope.counter= 1000;
+
+		$scope.$broadcast('timer-start');
+	}
+
 
 
   $scope.open = function () {
   var modalInstance = $uibModal.open({
     animation: $scope.animationsEnabled,
     templateUrl: './js/modalWindow/modalTimer.html',
+    scope: $scope,
     controller: 'ModalInstanceCtrl'
+  });
+
+  modalInstance.result.then(function (counter) {
+    $scope.counter = counter;
+    // $scope.$broadcast('timer-reset');
+    $scope.$broadcast('timer-start');
   });
 };
 
